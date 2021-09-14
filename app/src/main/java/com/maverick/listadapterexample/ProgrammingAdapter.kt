72573d81eb748319af733ff9b5464c685687d418
Maverick395006 <87902602+Maverick395006@users.noreply.github.com>
2021-09-14 16:30:56 +0530

    Function

---
+++ app/src/main/java/com/maverick/listadapterexample/ProgrammingAdapter.kt
@@ -0,0 +1,46 @@
+package com.maverick.listadapterexample
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+class ProgrammingAdapter:ListAdapter<ProgrammingItem,ProgrammingAdapter.ProgrammingViewholder>(DiffUtil()) {
+
+    class ProgrammingViewholder(view: View): RecyclerView.ViewHolder(view){
+        val name = view.findViewById<TextView>(R.id.name)
+        val initial = view.findViewById<TextView>(R.id.initial)
+
+        fun bind(item: ProgrammingItem) {
+            name.text = item.name
+            initial.text =item.initial
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProgrammingViewholder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_view,parent,false)
+        return ProgrammingViewholder(view)
+    }
+
+    override fun onBindViewHolder(holder: ProgrammingViewholder, position: Int) {
+        val item =getItem(position)
+        holder.bind(item)
+    }
+
+    class DiffUtil : androidx.recyclerview.widget.DiffUtil.ItemCallback<ProgrammingItem>() {
+        override fun areItemsTheSame(oldItem: ProgrammingItem, newItem: ProgrammingItem): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(
+            oldItem: ProgrammingItem,
+            newItem: ProgrammingItem
+        ): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+}
---
+++ app/src/main/java/com/maverick/listadapterexample/ProgrammingItem.kt
@@ -0,0 +1,3 @@
+package com.maverick.listadapterexample
+
+data class ProgrammingItem(val id: Int,val initial: String, val name: String)
